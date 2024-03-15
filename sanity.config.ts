a9b124f7c1bec6e0513714f897374e289ed6f124
Sufian mustafa <sufianmustafa0900@gmail.com>
2024-03-15 21:37:35 +0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "@/sanity/schemas";
+import { schemaTypes } from "./src/sanity/schemas";
 import StudioHeader from "@/components/StudioHeader";
+import { codeInput } from "@sanity/code-input";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/studio",
-  name: "OREBI_Shopping",
-  title: "OREBI online shopping",
+  name: "default",
+  title: "sanity_ecom",
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), codeInput()],
   schema: {
     types: schemaTypes,
   },
